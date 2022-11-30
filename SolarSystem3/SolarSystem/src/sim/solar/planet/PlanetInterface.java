6ccaa6016f3a1e1b32354fab48b1b51396a0a789
mythri-reddie <101601113+mythri-reddie@users.noreply.github.com>
2022-11-29 23:45:16 -0500

    removed unnecessary modifiers

--- SolarSystem3/SolarSystem/src/sim/solar/planet/PlanetInterface.java
+++ SolarSystem3/SolarSystem/src/sim/solar/planet/PlanetInterface.java
@@ -3,10 +3,10 @@ package sim.solar.planet;
 import java.awt.Color; 
 
 public interface PlanetInterface {
-  public int GetX() ;    
-  public int GetY() ; 
-  public int GetSize()  ;  
-  public Color GetColor() ;
-  public void run() ;
+   int GetX() ;    
+   int GetY() ; 
+   int GetSize()  ;  
+   Color GetColor() ;
+   void run() ;
 }
 
