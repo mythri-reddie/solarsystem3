6571abfcaea8a35268706bf550b75a298eff8650
mythri-reddie <101601113+mythri-reddie@users.noreply.github.com>
2022-11-29 23:48:40 -0500

    removed unnecessary modifiers

--- SolarSystem3/SolarSystem/src/sim/solar/planet/NurseryInterface.java
+++ SolarSystem3/SolarSystem/src/sim/solar/planet/NurseryInterface.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public interface  NurseryInterface {
 
-    public ArrayList<PlanetInterface> Produce () ;
-    public String GetAuthor ()  ;   
-    public String GetTitle () ;    
+     ArrayList<PlanetInterface> Produce () ;
+     String GetAuthor ()  ;   
+     String GetTitle () ;    
 }
