2910691d72ed6e125c9b825a8f9c5b6a4dede5a9
mythri-reddie <101601113+mythri-reddie@users.noreply.github.com>
2022-11-29 23:22:05 -0500

    added default serial version ID

--- SolarSystem3/SolarSystem/src/sim/solar/DisplayFrame.java
+++ SolarSystem3/SolarSystem/src/sim/solar/DisplayFrame.java
@@ -3,6 +3,7 @@ package sim.solar;
 import javax.swing.JFrame;
 
 public class DisplayFrame extends JFrame {
+  private static final long serialVersionUID = 1L;
   public void Activate ()  {
   
     setDefaultCloseOperation(EXIT_ON_CLOSE);
